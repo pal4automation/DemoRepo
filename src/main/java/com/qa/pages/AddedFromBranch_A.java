c2067fc6db5f50752b000e0bdf0fa5c9e51a8b29
Sanjeet <sanjeet.pal@creditvidya.com>
2018-02-09 15:31:21 +0530

    Added new test case.

--- src/main/java/com/qa/pages/AddedFromBranch_A.java
+++
@@ -1,9 +0,0 @@
-package com.qa.pages;
-
-public class AddedFromBranch_A {
-
-    public  void methodofBranchA(){
-        System.out.println("This method is added from Branch A");
-    }
-
-}
---
+++ src/main/java/com/qa/pages/ClassFromRefactorB.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class ClassFromRefactorB {
+
+    public void methodFromBranchB(){
+        System.out.println("This method is from BranchB");
+    }
+}
